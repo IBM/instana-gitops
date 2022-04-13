e9dd6413e16dcb2bf1545585bc4b9794d8a14ce2
lihongbj <lihongbj@cn.ibm.com>
2022-04-13 11:55:58 +0800

    tidy prepare

--- config/instana-operator/scripts/prepare.sh
+++ config/instana-operator/scripts/prepare.sh
@@ -1,33 +1,11 @@
 #/bin/bash
 
-# precheck
-if [ "$INSTANA_SALES_KEY" = "" ] ; then
-	echo "env variable INSTANA_SALES_KEY must be set in advance. exit now."
-	exit 1
-fi
-
 # create ns instana-operator to create other stuff: secret/kubeconfig
 oc create ns instana-operator
-
-# prepare license from instana
-# kubectl instana license download --sales-key $INSTANA_SALES_KEY
-# cat license.json | tr -d '[]"' > license
-# oc create configmap instana-license -n default --from-file=lic=license
 
 # kubeconfig
 oc create secret generic kubeconfig --from-file=credentials=$HOME/.kube/config -n instana-operator
 
-
 # cert-manager
-# ?? if not apply in advance, ??
 oc apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.0/cert-manager.yaml
-
-# optional ??
-# if cm/instana-sppem, then create 
-# create key.pem, cert.pem
-#mycertpem.sh $portalPassword  $base
-#cat key.pem cert.pem > sp.pem
-#kubectl create configmap instana-sppem -n default --from-file=sppem=sp.pem
-
-
 
